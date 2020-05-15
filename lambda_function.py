2a309c3b516de12b0ba0799edf8fdd263c9993d2
MarkGelo <mlgameng@gmail.com>
2020-05-15 17:52:31 -0500

    updated lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -16,13 +16,15 @@ import time
 
 # LOGIN
 # get required info from environment variables
+# more secure
 username = os.environ['username']
 cid = os.environ['cid']
 secret = os.environ['secret']
 redirect_uri = os.environ['redirect_uri']
 refreshToken = os.environ['refresh_token'] # do i need this
+last_fm_api_key = os.environ['last_fm_api_key']
 url = "https://accounts.spotify.com/api/token"
-
+USER_AGENT = "GenreGetter-geloprojects"
 # HOW TO login AND GET ACCESS TOKEN HMMMMM
 #for avaliable scopes see https://developer.spotify.com/web-api/using-scopes/
 scope = 'user-library-read playlist-modify-public playlist-read-private'
@@ -35,8 +37,16 @@ token = ''
 #to dynamodb
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table('Spotify')
+genreTable = dynamodb.Table('SpotifyGenre')
 response = table.get_item(Key = {'user' : 'MxMk'})
 token = response['Item']['accessToken']
+
+today = date.today()
+# dd/mm/YY
+dateToday = today.strftime("%d/%m/%Y")
+
+outStatusCode = 200 # default status code, returns successfulyl, 222 if error in playlist update
+outResponse = '' # default body of output of lambda function
 # get new access token using refresh token
 def refresh_access_token():
     #https://developer.spotify.com/documentation/general/guides/authorization-guide/
@@ -86,7 +96,44 @@ def roundDownToTens(x):
 def roundDown(n, decimals = 0): # decimals is where to round down to, if 0, then just integer, 2.4 -> 2, if 1, then 1.37 -> 1.3
     multiplier = 10 ** decimals
     return math.floor(n * multiplier) / multiplier
-    
+
+def get_genre(id, songName, artist):
+    # only one artist name, might skew the search a bit but whatev
+    genres = []
+    # check if id in dynamodb, if so dont do any of this
+    checkSong = genreTable.get_item(Key = {'songID' : id})
+    try:
+        genresInTable = checkSong['Item']['genres']
+        return genresInTable # already have genre, or already tried to find using last fm api so return
+    except: # not in table, so follow through with this method
+        pass
+    time.sleep(1) # seconds for each call
+    fmheaders = {"user-agent" : last_fm_api_key}
+    fmpayload = {
+        'api_key': last_fm_api_key,
+        'method': 'track.getTopTags',
+        'track' : songName,
+        'artist' : artist,
+        'format': 'json'
+    }
+    r = requests.get('http://ws.audioscrobbler.com/2.0/', headers=fmheaders, params=fmpayload)
+    data = r.json()
+    try:
+        tags = data['toptags']['tag']
+        # get top 5 tags
+        for i in range(0, 5):
+            genres.append(tags[i]['name'].lower()) # turns all to lowercase in case tags might have different capitilizaitons but still same tag
+    except: # error in finding the track
+        pass # nothing so empty list
+    # if got the tags, save to dynamodb
+    # even if didnt get anything, save id in database
+    # so it doesnt go through all of this again each time to result with nothing again
+    if len(genres) > 0:
+        genreTable.put_item(Item = {'songID' : id, 'genres': set(genres)})
+    else:
+        genreTable.put_item(Item = {'songID' : id, 'genres' : set(['NotFound'])})
+    return genres
+
 def get_generated_playlists(sp):
     # gets saved songs, while also making list for the popularity and year playlists to update the playlists
     saved = sp.current_user_saved_tracks()
@@ -98,6 +145,17 @@ def get_generated_playlists(sp):
         for song in saved['items']:
             savedSongs.append(song['track']['id'])
             i += 1
+            # GET GENRE
+            songName = song['track']['name']
+            songArtist = song['track']['artists'][0]['name'] # get first artist
+            genres = get_genre(song['track']['id'], songName, songArtist)
+            time.sleep(0.05)
+            for genre in genres:
+                # adds to dict
+                if genre in generated['Genre']:
+                    generated['Genre'][genre].append(song['track']['id'])
+                else:
+                    generated['Genre'][genre] = [song['track']['id']]
             # GET YEARS
             year = 0 # unknown default if somehow couldnt parse or whatev
             # parses release date and rounds down to 10s
@@ -119,9 +177,6 @@ def get_generated_playlists(sp):
                 generated['Popularity'][pop].append(song['track']['id']) # if its already there, appends to the current list
             else: # if not , then makes one and with a list
                 generated['Popularity'][pop] = [song['track']['id']]
-            
-            # GET GENRE HOW THO
-            # ------------------------------------------------------
 
     # GET AUDIO STATS
     # https://developer.spotify.com/documentation/web-api/reference/tracks/get-audio-features/
@@ -377,6 +432,157 @@ def update_characteristic_playlists(sp, generated, dateToday):
     sp.user_playlist_change_details(username, happy, 
                                     description = 'Valence >= 0.8 -- Updated on {}'.format(dateToday))
 
+def update_genre_playlist(sp, generated, dateToday, playlistInfo):
+    playlistName = playlistInfo['name']
+    playlistGenres = playlistInfo['genres']
+    # UPDATE Hip-hop?
+    playlist = getUserPlaylistID(sp, playlistName)
+    if playlist is False:
+        print('Unable to get playlist ID')
+        # return an error
+        exit()
+    songsToAdd = []
+    for genre in playlistGenres:
+        songsToAdd.extend(generated['Genre'][genre])
+    songsToAdd1 = list(dict.fromkeys(songsToAdd)) # removes duplicates
+    toAdd = divideList(songsToAdd1, 100)
+    sp.user_playlist_replace_tracks(username, playlist, []) # clears playlist
+    for songs in toAdd:
+        sp.user_playlist_add_tracks(username, playlist, songs)
+    sp.user_playlist_change_details(username, playlist, 
+                                    description = '{} on Last.fm -- Updated on {}'.format(', '.join(playlistGenres), dateToday))
+
+def update_playlist(sp, generated, playlistInfo):
+    playlistName = playlistInfo['name']
+    playlistID = getUserPlaylistID(sp, playlistName)
+    global outResponse, outStatusCode
+    if playlistID is False:
+        outResponse += 'Unable to get playlist ID of {}\n'.format(playlistName)
+        outStatusCode = 222 # idk made up status code lol
+        return
+    # default gate
+    gate = 'or'
+    if 'gate' in playlistInfo:
+        gate = playlistInfo['gate']
+    # get description info
+    descr = [] # default none
+    toAdd = []
+    if 'genres' in playlistInfo:
+        for genre in playlistInfo['genres']:
+            descr.append(genre)
+            if gate == 'or':
+                toAdd.extend(generated['Genre'][genre])
+            else: # and, so track has to have all these genres
+                toAdd = list(set(toAdd) & set(generated['Genre'][genre])) # intersection
+    if 'years' in playlistInfo:
+        for year in playlistInfo['years']:
+            descr.append('{}s'.format(year))
+            if gate == 'or':
+                toAdd.extend(generated['Year'][year])
+            else:
+                toAdd = list(set(toAdd) & set(generated['Year'][year]))
+    if 'characteristics' in playlistInfo:
+        for characteristic in playlistInfo['characteristics']:
+            restr = playlistInfo['characteristics'][characteristic].split(' ')
+            descr.append('{} {}'.format(characteristic, ' '.join(restr)))
+            for numbers in generated['Audio'][characteristic]:
+                if len(restr) == 3: # x > 5
+                    if restr[1] == '>':
+                        if numbers >= float(restr[2]):
+                            if gate == 'or':
+                                toAdd.extend(generated['Audio'][characteristic][numbers])
+                            else:
+                                toAdd = list(set(toAdd) & set(generated['Audio'][characteristic][numbers]))
+                    elif restr[1] == '<':
+                        if numbers < float(restr[2]):
+                            if gate == 'or':
+                                toAdd.extend(generated['Audio'][characteristic][numbers])
+                            else:
+                                toAdd = list(set(toAdd) & set(generated['Audio'][characteristic][numbers]))
+                    else:
+                        outResponse += 'Wrong formatting of characteristic info of playlist'
+                        outStatusCode = 222
+                        return
+                elif len(restr) == 5: # 5 < x < 6
+                    if restr[1] == '<' and restr[3] == '<':
+                        if numbers >= float(restr[0]) and numbers < float(restr[4]):
+                            if gate == 'or':
+                                toAdd.extend(generated['Audio'][characteristic][numbers])
+                            else:
+                                toAdd = list(set(toAdd) & set(generated['Audio'][characteristic][numbers]))
+                    elif restr[1] == '>' and restr[3] == '>':
+                        if numbers < float(restr[0]) and numbers >= float(restr[4]):
+                            if gate == 'or':
+                                toAdd.extend(generated['Audio'][characteristic][numbers])
+                            else:
+                                toAdd = list(set(toAdd) & set(generated['Audio'][characteristic][numbers]))
+                    else:
+                        outResponse += 'Wrong formatting of characteristic info of playlist'
+                        outStatusCode = 222
+                        return
+                else:
+                    outResponse += 'Wrong formatting of characteristic info of playlist'
+                    outStatusCode = 222
+                    return
+    if 'popularity' in playlistInfo:
+        restr = playlistInfo['popularity'].split(' ')
+        descr.append('Popularity {}'.format(' '.join(restr)))
+        for numbers in generated['Popularity']:
+            if len(restr) == 3: # x > 5
+                if restr[1] == '>':
+                    if numbers >= float(restr[2]):
+                        if gate == 'or':
+                            toAdd.extend(generated['Popularity'][numbers])
+                        else:
+                            toAdd = list(set(toAdd) & set(generated['Popularity'][numbers]))
+                elif restr[1] == '<':
+                    if numbers < float(restr[2]):
+                        if gate == 'or':
+                            toAdd.extend(generated['Popularity'][numbers])
+                        else:
+                            toAdd = list(set(toAdd) & set(generated['Popularity'][numbers]))
+                else:
+                    outResponse += 'Wrong formatting of characteristic info of playlist'
+                    outStatusCode = 222
+                    return
+            elif len(restr) == 5: # 5 < x < 6
+                if restr[1] == restr[3] and restr[3] == '<':
+                    if numbers >= float(restr[0]) and numbers < float(restr[4]):
+                        if gate == 'or':
+                            toAdd.extend(generated['Popularity'][numbers])
+                        else:
+                            toAdd = list(set(toAdd) & set(generated['Popularity'][numbers]))
+                elif restr[1] == restr[3] and restr[3] == '>':
+                    if numbers < float(restr[0]) and numbers >= float(restr[4]):
+                        if gate == 'or':
+                            toAdd.extend(generated['Popularity'][numbers])
+                        else:
+                            toAdd = list(set(toAdd) & set(generated['Popularity'][numbers]))
+                else:
+                    outResponse += 'Wrong formatting of characteristic info of playlist'
+                    outStatusCode = 222
+                    return
+            else:
+                outResponse += 'Wrong formatting of characteristic info of playlist'
+                outStatusCode = 222
+                return
+    #remove duplicates
+    toAdd1 = list(dict.fromkeys(toAdd))
+    # update playlist
+    songsToAdd = divideList(toAdd1, 100)
+    sp.user_playlist_replace_tracks(username, playlistID, []) # clears playlist
+    for songs in songsToAdd:
+        sp.user_playlist_add_tracks(username, playlistID, songs)
+        time.sleep(2)
+    if gate == 'or':
+        descr = '|'.join(descr)
+    elif gate == 'and':
+        descr = ','.join(descr)
+    else:
+        descr = '/'.join(descr)
+    sp.user_playlist_change_details(username, playlistID, 
+                                    description = '{} -- Updated on {}'.format(descr, dateToday))
+    
 def lambda_handler(event, context):
     spotify = login() # spotify obj
     generated = get_generated_playlists(spotify)
@@ -384,12 +590,51 @@ def lambda_handler(event, context):
     # added multiple sleep 6 seconds in the methods, so takes like 2 mins
     # so doesnt reach api rate limit
     # might be overkill but better to be safe than not
+    
+    # playlists to update, with information on what to have in playlist
+    # has name, genres, years, popularity, characteristics, gate ('and' or 'or') -- logic gate
+    # str, [str], [int], str, {'Tempo' : str, ... : str}, str
+    # gate is for .. example: genres = ['rnb', 'soul'] gate = 'and' ---- default gate is 'or'
+    # returns playlist with songs with both genre rnb and soul
+    # charactersitic ex: {'Tempo' : ' x > 140'
+    # x has to be leftmost
+    playlists = [
+        {'name': 'Underrated?', 'popularity': '10 < x < 40'},
+        {'name': 'Unknown?', 'popularity': 'x < 10'},
+        {'name': 'Quiet?', 'characteristics': {'Loudness': 'x < -16.0'}},
+        {'name': 'Danceable?', 'characteristics': {'Danceability': 'x > 0.8'}},
+        {'name': 'Low Energy?', 'characteristics': {'Energy': 'x < 0.3'}},
+        {'name': 'Sad?', 'characteristics': {'Valence': 'x < 0.20'}},
+        {'name': 'High Energy?', 'characteristics': {'Energy': 'x > 0.9'}},
+        {'name': 'No Vocals?', 'characteristics': {'Instrumentalness': 'x > 0.7'}},
+        {'name': 'High Tempo?', 'characteristics': {'Tempo': 'x > 150'}},
+        {'name': 'Happy?', 'characteristics': {'Valence': 'x > 0.8'}},
+        {'name': '00s', 'years': [2000]},
+        {'name': '90s', 'years': [1990]},
+        {'name': '80s', 'years': [1980]},
+        {'name': '70s', 'years': [1970]},
+        {'name': '60s', 'years': [1960]},
+        {'name': 'Hip-hop?', 'genres': ['hip-hop']}, # genres are all lowercase
+        {'name': 'Rnb?', 'genres': ['rnb']},
+        {'name': 'Soul?', 'genres': ['soul']},
+        {'name': 'Rock?', 'genres': ['rock']},
+        {'name': 'Electronic?', 'genres': ['electronic']},
+        {'name': 'Indie?', 'genres': ['indie']},
+        {'name': 'Alternative?', 'genres': ['alternative']},
+        {'name': 'Pop?', 'genres': ['pop']}
+        ]
     today = date.today()
     # dd/mm/YY
     dateToday = today.strftime("%d/%m/%Y")
-    update_basic_playlists(spotify, generated, dateToday)
-    update_characteristic_playlists(spotify, generated, dateToday)
+    
+    for playlist in playlists:
+        update_playlist(spotify, generated, playlist)
+        time.sleep(5)
+
+    global outResponse, outStatusCode
+    if outResponse == '':
+        outResponse = 'Successfully updated playlists'
     return{
-        'statusCode': 200,
-        'body': json.dumps('Success')
+        'statusCode': outStatusCode,
+        'body': json.dumps(outResponse)
     }
